430f108f9fadca36894f66b7fb50a7eb3d7370ff
Rohit <rohitshetty4334@gmail.com>
2026-01-20 15:13:16 +0530

    Update exp1day1.py

--- exp1day1.py
+++ exp1day1.py
@@ -4,7 +4,7 @@ from groq import Groq
 st.set_page_config("PragyanAI Content Generator", layout="wide")
 st.title("📢 PragyanAI – Content Generator")
 
-client = Groq(api_key=st.secrets["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"])
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 col1, col2 = st.columns(2)
 
